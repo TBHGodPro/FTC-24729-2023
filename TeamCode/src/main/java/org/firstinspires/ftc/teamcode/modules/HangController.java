48e7baf6fab4279c3d2a2cc9a06dfafcac8c510a
Guest <tarun.bhaskar@outlook.com>
2024-01-22 18:29:41 -0600

    auto hang?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/HangController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/HangController.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.modules;
 
+import com.acmerobotics.dashboard.config.Config;
+
 import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -7,13 +9,27 @@ import org.firstinspires.ftc.teamcode.MainOp;
 import org.firstinspires.ftc.teamcode.modules.Arm.ArmController;
 import org.firstinspires.ftc.teamcode.modules.Inputs.InputManager;
 
+@Config
 public class HangController extends BaseModule {
+    // --- Constants ---
+    
+    public static double holdPitch = 33;
+    public static double holdAccuracy = 6;
+    
+    public static double liftPower = 1.2;
+    public static double lowerPower = 0.3;
+    public static double holdPower = 0.5;
+    
+    // -----------------
+    
     public final MovementController movements;
     public final ArmController arm;
     public final InputManager inputs;
     
     public boolean lastHangToggle = false;
     public boolean active = false;
+    
+    public double basePitch;
     
     public HangController(MainOp op, InputManager inputs) {
         super(op);
@@ -33,15 +49,30 @@ public class HangController extends BaseModule {
             active = !active;
             
             if (active) {
-                arm.setArmStaticPower(-0.25);
+                basePitch = movements.imu.getRobotYawPitchRollAngles().getPitch(AngleUnit.DEGREES);
+                
                 movements.deactivate();
-                arm.wristPos = 100;
+                //arm.wristPos = 100;
             } else {
                 arm.setArmStaticPower(0);
                 movements.activate();
             }
         } else {
             lastHangToggle = false;
+        }
+        
+        if (active) {
+            movements.wheelController.setPowers(0, 0, 0, 0);
+            
+            double pitch = movements.imu.getRobotYawPitchRollAngles().getPitch(AngleUnit.DEGREES);
+            
+            double realPitch = pitch - basePitch;
+            
+            if (Math.round(realPitch / holdAccuracy) == Math.round(holdPitch / holdAccuracy))
+                arm.setArmStaticPower(-holdPower);
+            else if (realPitch < holdPitch) arm.setArmStaticPower(-liftPower);
+            else if (realPitch > holdPitch) arm.setArmStaticPower(-lowerPower);
+            else arm.setArmStaticPower(-holdPower);
         }
     }
     
